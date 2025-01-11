4b71df494f1f704ca92dfb3a1010c29a0dc3a795
thomaskavi <thomaskavi2000@gmail.com>
2025-01-11 13:18:55 -0300

    First upload test AWS3

--- dscatalog/backend/src/main/java/com/thomaskavi/dscatalog/DscatalogApplication.java
+++ dscatalog/backend/src/main/java/com/thomaskavi/dscatalog/DscatalogApplication.java
@@ -1,13 +1,26 @@
 package com.thomaskavi.dscatalog;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.thomaskavi.dscatalog.services.S3Service;
+
 @SpringBootApplication
-public class DscatalogApplication {
+public class DscatalogApplication implements CommandLineRunner {
+
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DscatalogApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		s3Service.uploadFile("C:\\tmp\\print.jpg");
 	}
 
 }
